c4c5c3f6f40f1e2b4930f8f55f8698ebb7fc3858
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-22 18:28:34 +0200

    commit

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,15 @@ class Nodo:
         self.izquierda = izquierda
         self.derecha = derecha
 
+class ArbolDecision:
+    def __init__(self):
+        # Crear nodos hoja con los superhéroes
+        iron_man = Nodo(superheroe="Iron Man")
+        hulk = Nodo(superheroe="Hulk")
+        khan = Nodo(superheroe="Khan")
+        thor = Nodo(superheroe="Thor")
+        capitan_america = Nodo(superheroe="Captain América")
+        capitana_marvel = Nodo(superheroe="Capitana Marvel")
+        ant_man = Nodo(superheroe="Ant-Man")
+        nick_fury = Nodo(superheroe="Nick Fury")
+        winter_soldier = Nodo(superheroe="The Winter Soldier")
