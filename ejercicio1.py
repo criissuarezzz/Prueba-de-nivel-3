c3e28f76ab8c497968ec53e69c060afd9fe5f8c7
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-22 18:28:49 +0200

    commit

--- ejercicio1.py
+++ ejercicio1.py
@@ -17,3 +17,14 @@ class ArbolDecision:
         ant_man = Nodo(superheroe="Ant-Man")
         nick_fury = Nodo(superheroe="Nick Fury")
         winter_soldier = Nodo(superheroe="The Winter Soldier")
+
+        # Crear nodos intermedios con preguntas
+        pregunta_vuela = Nodo(pregunta="¿Puede volar?", izquierda=iron_man, derecha=hulk)
+        pregunta_fuerza = Nodo(pregunta="¿Tiene mucha fuerza?", izquierda=khan, derecha=pregunta_vuela)
+        pregunta_invisibilidad = Nodo(pregunta="¿Puede volverse invisible?", izquierda=ant_man, derecha=nick_fury)
+        pregunta_inteligencia = Nodo(pregunta="¿Es muy inteligente?", izquierda=pregunta_fuerza, derecha=pregunta_invisibilidad)
+        pregunta_lider = Nodo(pregunta="¿Es un buen líder?", izquierda=pregunta_inteligencia, derecha=thor)
+        pregunta_etica = Nodo(pregunta="¿Tiene una ética incorruptible?", izquierda=capitan_america, derecha=capitana_marvel)
+        pregunta_recuperacion = Nodo(pregunta="¿Es bueno en misiones de recuperación?", izquierda=winter_soldier, derecha=pregunta_lider)
+
+       
