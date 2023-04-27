692716d637c6af1bbc65467ecd40436fd210ab8c
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-27 19:17:13 +0200

    commit

--- ejercicio6.py
+++ ejercicio6.py
@@ -6,29 +6,7 @@ class Carta:
         self.numero = numero
         self.palo = palo
 
-# Generar la pila de cartas aleatoriamente
-mazo = []
-for numero in range(1, 13):
-    for palo in ['espada', 'basto', 'copa', 'oro']:
-        carta = Carta(numero, palo)
-        mazo.append(carta)
-random.shuffle(mazo)
-
-# Crear las pilas correspondientes a cada palo
-espada = []
-basto = []
-copa = []
-oro = []
-
-# Separar la pila de cartas en las pilas correspondientes a cada palo
-while mazo:
-    carta = mazo.pop()
-    if carta.palo == 'espada':
-        espada.append(carta)
-    elif carta.palo == 'basto':
-        basto.append(carta)
-    elif carta.palo == 'copa':
-        copa.append(carta)
-    else:
-        oro.append(carta)
-        
+    def __str__(self):
+        return str(self.numero) + " de " + self.palo
+    
+    
