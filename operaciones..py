9ea76b1904e5b10b1ae37438dcc7f8a08011009c
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-27 08:47:45 +0200

    commit

---
+++ operaciones..py
@@ -0,0 +1,13 @@
+class Operaciones:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def suma(self):
+        try:
+            result= self.num1 + self.num2
+            return result
+        except TypeError:
+            print("Error: Tipo de dato no válido")
+
+                
