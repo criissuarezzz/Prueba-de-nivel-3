134a7e52d48ee9cda011ead3d93c22fd37527949
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-27 19:18:24 +0200

    commit

--- ejercicio6.py
+++ ejercicio6.py
@@ -9,4 +9,23 @@ class Carta:
     def __str__(self):
         return str(self.numero) + " de " + self.palo
     
+class Pila:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+    
+    def apilar(self, item):
+        self.items.append(item)
+
+    def desapilar(self):
+        if self.esta_vacia():
+            raise IndexError("La pila está vacía")
+        return self.items.pop()
+    
+    def ver_tope(self):
+        if self.esta_vacia():
+            raise IndexError("La pila está vacía")
+        return self.items[-1]
     
