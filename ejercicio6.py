f0644cf8f8e4ce19706cd4086c2c517340e777ff
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-04-27 19:29:06 +0200

    commit

--- ejercicio6.py
+++ ejercicio6.py
@@ -35,52 +35,51 @@ class Pila:
     def __str__(self):
         return str([str(item) for item in self.items])
     
-class Mazo:
-    mazo=Pila()
-    palo_espada=Pila()
-    palo_basto=Pila()
-    palo_oro=Pila()
-    palo_copa=Pila()
 
-    def generar_aleatoria():
-        for i in range(1,13):
-            for palo in ["Espada","Basto","Oro","Copa"]:
-                carta=Carta(i,palo)
-                Mazo.mazo.apilar(carta)
-        random.shuffle(Mazo.mazo.items)
-
-    def separar():
-        while not Mazo.mazo.esta_vacia():
-            carta=Mazo.mazo.desapilar()
-            if carta.palo=="Espada":
-                Mazo.palo_espada.apilar(carta)
-            elif carta.palo=="Basto":
-                Mazo.palo_basto.apilar(carta)
-            elif carta.palo=="Oro":
-                Mazo.palo_oro.apilar(carta)
-            else:
-                Mazo.palo_copa.apilar(carta)
-
-    def ordenar():
-        ordenado=Pila()
-        while not Mazo.palo_espada.esta_vacia():
-            carta=Mazo.palo_espada.desapilar()
-            while not ordenado.esta_vacia() and ordenado.ver_tope().numero > carta.numero:
-                Mazo.palo_espada.apilar(ordenado.desapilar())
-            Mazo.ordenado.apilar(carta)
-        while not ordenado.esta_vacia():
-            Mazo.palo_espada.apilar(ordenado.desapilar())
-
-    def imprimir():
-        print("Espada: ",Mazo.palo_espada)
-        print("Basto: ",Mazo.palo_basto)
-        print("Oro: ",Mazo.palo_oro)
-        print("Copa: ",Mazo.palo_copa)
 
 if __name__=='__main__':
-    Mazo.generar_aleatoria()
-    Mazo.separar()
-    Mazo.ordenar()
-    Mazo.imprimir()
+    mazo = Pila()
+    palo_espada = Pila()
+    palo_basto = Pila()
+    palo_copa = Pila()
+    palo_oro = Pila()
 
-    
+    # Generar el mazo de cartas de manera aleatoria
+    for i in range(1, 13):
+        for palo in ["espada", "basto", "copa", "oro"]:
+            carta = Carta(i, palo)
+            mazo.apilar(carta)
+            
+    random.shuffle(mazo.items)
+
+    # Separar el mazo en cuatro pilas una por cada palo
+    while not mazo.esta_vacia():
+        carta = mazo.desapilar()
+        if carta.palo == "espada":
+            palo_espada.apilar(carta)
+        elif carta.palo == "basto":
+            palo_basto.apilar(carta)
+        elif carta.palo == "copa":
+            palo_copa.apilar(carta)
+        else:
+            palo_oro.apilar(carta)
+
+    # Ordenar la pila de espadas de manera creciente por número        
+    ordenado = Pila()
+    while not palo_espada.esta_vacia():
+        carta = palo_espada.desapilar()
+        while not ordenado.esta_vacia() and ordenado.ver_tope().numero > carta.numero:
+            palo_espada.apilar(ordenado.desapilar())
+        ordenado.apilar(carta)
+    while not ordenado.esta_vacia():
+        palo_espada.apilar(ordenado.desapilar())
+
+    # Imprimir las cuatro pilas
+    print("Palo de Espada:", palo_espada)
+    print("\n")
+    print("Palo de Basto:", palo_basto)
+    print("\n")
+    print("Palo de Copa:", palo_copa)    
+    print("\n")
+    print("Palo de Oro:", palo_oro)    
+    print("\n")
